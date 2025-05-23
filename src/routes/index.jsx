a0574b6379c4470c04b6d94de3a2668a58c7d437
ErickTorres6412 <erick.torres.hernandez@est.una.ac.cr>
2025-05-23 13:09:37 -0600

    Plantilla actualizada con solo login

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,20 +4,7 @@ import Layout from '../components/Layout/Layout';
 
 // Importa los componentes de páginas
 import Dashboard from '../pages/Dashboard/Dashboard';
-import VisualizarCondominios from '../pages/Configuracion/Condominios/VisualizarCondominios';
-import CrearCondominio from '../pages/Configuracion/Condominios/CrearCondominio';
-import CrearAreaComun from '../pages/Configuracion/AreaComun/CrearAreaComun';
-import VisualizarAreaComun from '../pages/Configuracion/AreaComun/VisualizarAreaComun';
-import CrearVehiculo from '../pages/Configuracion/Vehiculos/CrearVehiculo';
-import VisualizarVehiculos from '../pages/Configuracion/Vehiculos/VisualizarVehiculos';
-import Foro from '../pages/Foro/Foro';
-import CrearTemaForo from '../pages/Foro/CrearTemaForo';
-import Login from '../pages/Login/Login'; 
-import DetalleTema from '../pages/Foro/DetalleTema';
-import CrearSector from '../pages/Configuracion/Sector/CrearSector';
-import VisualizarSector from '../pages/Configuracion/Sector/VisualizarSector'
-import CrearPropiedad from '../pages/Configuracion/Propiedad/CrearPropiedad'; 
-import VisualizarPropiedad from '../pages/Configuracion/Propiedad/VisualizarPropiedad';
+import Login from '../pages/Login/Login';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,20 +16,6 @@ const router = createBrowserRouter(
       {/* Todo lo demás, protegido con Layout */}
       <Route element={<Layout />}>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/visualizar-condominios" element={<VisualizarCondominios />} />
-        <Route path="/crear-condominio" element={<CrearCondominio />} />
-        <Route path="/areas/crear" element={<CrearAreaComun />} />
-        <Route path="/areas/ver" element={<VisualizarAreaComun />} />
-        <Route path="/sector/ver" element={<VisualizarSector />} />
-        <Route path="/sector/crear" element={<CrearSector/>} />
-        <Route path="/propiedad/ver" element={<VisualizarPropiedad />} />
-        <Route path="/propiedad/crear" element={<CrearPropiedad/>} />
-        <Route path="/vehiculos/crear" element={<CrearVehiculo />} />
-        <Route path="/vehiculos/visualizar" element={<VisualizarVehiculos />} />
-        <Route path="/foro" element={<Foro />} />
-        <Route path="/foro/crear" element={<CrearTemaForo />} />
-        <Route path="/foro/:foroID" element={<DetalleTema />} /> {/* Detalle del tema del foro */}
-        {/* Aquí puedes agregar más rutas según sea necesario */}
       </Route>
 
       {/* Más rutas a futuro aquí */}
